ebd712ebb4db17ba365ef8a8aad296631438952b
ses10 <sesma@live.com>
2015-06-06 15:36:31 -0400

    add code to allow mainstream and chat to change according to stream preview clicked

--- Twitch Counter-Strike/public_html/js/script.js
+++ Twitch Counter-Strike/public_html/js/script.js
@@ -9,18 +9,20 @@ $(document).ready(function(){
         success: function(response){
             
             streamUrl = "http://www.twitch.tv/widgets/live_embed_player.swf?channel=";
+            streamChatUrl = ["http://www.twitch.tv/", "/chat?popout="];
             streams = response.streams;
             
-            //set the main video stream to the first element in streams array
-            $('#stream-title').html(streams[0].channel.display_name);
-            $('#twitch-player').attr('data', streamUrl + streams[0].channel.display_name);
-            
+            //set the main video stream and chat to the first element in streams array
+            $('#stream-title').html(streams[0].channel.name);
+            $('#twitch-player').attr('data', streamUrl + streams[0].channel.name);
+            $('#chat iframe').attr('src', streamChatUrl[0] + streams[0].channel.name + streamChatUrl[1]);
+                        
             //loop through response and display stream previews
             for(var i = 0; i < streams.length; i++)
             {
                 $('#streamList').append(
-                        '<li class="streams" data-channel-name="'+ streams[i].channel.display_name +'">' +
-                            '<p>'+ streams[i].channel.display_name + '<br>' +
+                        '<li class="streams" data-channel-name="'+ streams[i].channel.name +'">' +
+                            '<p>'+ streams[i].channel.name + '<br>' +
                                    streams[i].channel.status.slice(0,40) + '<br>' +
                                    streams[i].viewers + ' viewing now' +
                             '</p>' +
@@ -33,8 +35,9 @@ $(document).ready(function(){
                 $(this).on("click", function(){
                     $('#stream-title').html($(this).attr("data-channel-name"));
                     $('#twitch-player').attr('data', streamUrl + $(this).attr("data-channel-name"));
+                    $('#chat iframe').attr('src', streamChatUrl[0] + $(this).attr("data-channel-name") + streamChatUrl[1]);
                 });
-            }); 
+            });     
         }
     });//End ajax request
     
